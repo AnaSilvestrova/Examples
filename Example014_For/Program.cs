441220242cd78c72eec1aeca56cfb5d201a2e485
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-19 02:50:38 +0700

    for added

---
+++ Example014_For/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Цикл for, или цикл со счетчиком.
+Example:
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine();
+}
+*/
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++) // Инициализация счетчика, проверка условия, инкремент. 
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "z"); // В "" текст, который будет "склеиваться" 10 раз.
+Console.WriteLine(res);
