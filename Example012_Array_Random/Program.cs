06e2972ce764dd8e825dcc2e8decb129001041c1
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-19 01:33:09 +0700

    void methods added

---
+++ Example012_Array_Random/Program.cs
@@ -0,0 +1,53 @@
+﻿// Решение с помощью генератора случайных чисел.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // Случайное число из диапазона 1-10.
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // Метод, который будет печатать массив.
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) // Для нахождения нужного индекса.
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // На тот случай, если искомого числа в массиве нет.
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // Создан новый массив из 10 элементов.
+
+FillArray(array); // Заполнение массива случайными числами.
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array); // Печать массива в консоли.
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine($"Нужный индекс --> {pos}");
