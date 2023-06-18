4a3ad2b0d1615057518242cc474b589a3b73f9a0
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-19 00:26:43 +0700

    1 array added

---
+++ Example010_Array_Max/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Нахождение максимума из 9 чисел.
+Массив array.
+*/
+
+int Max(int arg1, int arg2, int arg3) // Функция, сравнение по тройкам; аргументы.
+{
+    int result = arg1; // Тело метода
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result; 
+} // Вспомогательный механизм, который ищет максимум из ТРЕХ чисел.
+
+// Индексы       0   1   2   3   4   5   6   7   8
+int [] array = {11, 211, 31, 41, 15, 61, 17, 18, 19}; // Ввод массива, внутри {} значения элементов.
+int result = Max(
+   Max(array[0], array[1], array[2]), 
+   Max(array[3], array[4], array[5]),
+   Max(array[6], array[7], array[8])
+   );
+Console.WriteLine(result);
