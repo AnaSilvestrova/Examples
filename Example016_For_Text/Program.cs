3ce19f5a457ffe51cbafa367b4d6a8d43a1d9451
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-19 03:36:35 +0700

    text editor added

---
+++ Example016_For_Text/Program.cs
@@ -0,0 +1,40 @@
+﻿/* В тексте заменить все пробелы черточками, 
+маленькие буквы "к" заменить большими "К",
+маленькие "с" заменить большими "С".
+Текст дан отдельной строкой.
+Кириллица.
+*/
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне час?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue) // char - конкретный символ, тип переменной.
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
